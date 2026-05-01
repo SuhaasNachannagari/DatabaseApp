ea751e068021d29d32b2c6530c99a76938d5b818
Suhaas Nachannagari <madhusuhaas@gmail.com>
2026-05-01 16:24:46 -0400

    include indexing, injeciton protection, and concurerency

--- frontend/src/views/ReportView.vue
+++ frontend/src/views/ReportView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const API = 'http://127.0.0.1:5000'
+const API = 'https://databaseapp-68wc.onrender.com'
 
 const cuisineOptions = ref([])
 const ingredientOptions = ref([])
